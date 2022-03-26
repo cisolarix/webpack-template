b3957761c88769b753f584560f0bc9a0868553af
Yanming Deng <cisolarix@gmail.com>
2022-03-26 10:31:17 +0800

    initial

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  insertPragma: false,
+  // jsxBracketSameLine: true, // This option has been deprecated in v2.4.0, use --bracket-same-line instead
+  printWidth: 999,
+  proseWrap: "preserve",
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false,
+  arrowParens: "avoid",
+};
